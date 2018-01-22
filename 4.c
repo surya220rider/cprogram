8e4f41242eed9ee04addad8c33473c22ea1b1cae
surya220rider <30829445+surya220rider@users.noreply.github.com>
2018-01-22 15:53:59 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+char ch;
+printf("enter the letter:",ch);
+scanf("%d",&ch);
+{
+if((ch>=65&&ch<=90)||(ch >=92&&ch<=122))
+printf("%c is a alphet",ch);
+else
+printf("%c is not a alphet",ch);
+}
+}
