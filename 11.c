0af069c412d1ce6abac77ba8dd7f68d9ca14f7ca
surya220rider <30829445+surya220rider@users.noreply.github.com>
2018-01-22 16:12:26 +0530

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+int a,b;
+clrscr();
+printf("enter the value x");
+scanf("%d",&a);
+}
+printf("enter the value y");
+scanf("%d",&b);
+{
+for(i=0;i<0;i++)
+{
+int c=a+b;
+}
+}
