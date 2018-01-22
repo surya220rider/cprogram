9bb6f6544bdb13c4997cf1bae5d18021f0486ff7
surya220rider <30829445+surya220rider@users.noreply.github.com>
2018-01-22 16:07:26 +0530

    Create 9.c

---
+++ 9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <malloc.h>
+#include<conio.h>
+void duplicate(int array[], int num)
+{
+    int *count = (int *)calloc(sizeof(int), (num - 2));
+    int i;
+    printf("duplicate elements present in the array are ");
+    for (i = 0; i < num; i++)
+    {
+        if (count[array[i]] == 1)
+            printf(" %d ", array[i]);
+        else
+            count[array[i]]++;
+    }
+} 
+int main()
+{
+    int array[] = {5, 10, 10, 2, 1, 4, 2};
+    int array_freq = sizeof(array) / sizeof(array[0]);
+    duplicate(array, array_freq);
+    getchar();
+    getch();
+    }
