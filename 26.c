32ed49a5d8564a61773e5ec29833ac6468338094
surya220rider <30829445+surya220rider@users.noreply.github.com>
2018-01-31 16:29:41 +0530

    Create 26.c

---
+++ 26.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void main()
+{
+long int n,i,j,t;
+printf("enter the number");
+scanf("%ld",&n);
+long int a[n];
+for(i=0;i<n;i++)
+{
+scanf("%ld ",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+ if(a[i]>a[j])
+{
+ t=a[i];
+a[i]=a[j];
+a[j]=t;
+}
+}
+}
+for(i=0;i<n;i++)
+printf("%ld ",a[i]);
+}
