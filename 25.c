54a03b107e42797192e4372b3f3a1e1da1e4caec
surya220rider <30829445+surya220rider@users.noreply.github.com>
2018-01-31 16:09:10 +0530

    Create 25.c

---
+++ 25.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,j,m,a=[i],a[j];
+printf(enter the element");
+scanf("m",%d);
+printf("enter the number");
+for(i=0;i<m;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<m;i++)
+{
+for(j=i+1;j<m;j++)
+{
+if(a[i]>a[j])
+{
+t=a[i];
+a[i]=a[j];
+a[i]=t;
+}
+print"median element",a[m/2]);
+}
+}
+}
+return 0;
+}
