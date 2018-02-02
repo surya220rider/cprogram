14c4d7bfb1d05d21306f62da193fe687dcb92638
surya220rider <30829445+surya220rider@users.noreply.github.com>
2018-02-02 15:52:00 +0530

    Create 28.c

---
+++ 28.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int i,a[10],n;
+printf("enter the number:");
+scanf("%d",&n);
+printf("enter %d number:",n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[10]);
+}
+for(i=0;i<n;i++)
+{
+printf("%d%d\n",a[i],i);
+}
+return 0;
+}
