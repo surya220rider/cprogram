d3f7aa965c4e13375d242dcd01d1302caea55f2a
surya220rider <30829445+surya220rider@users.noreply.github.com>
2018-02-07 16:18:29 +0530

    Update 30.c

--- 30.c
+++ 30.c
@@ -1,1 +1,18 @@
-
+#include <stdio.h>
+ void main()
+{
+int input,hour,min,sec;
+printf("input number in total seconds\n");
+scanf("%d", &input_v);
+while(input > 3600)
+{
+hour = input /3600;
+min = input % 60;
+sec = min % 60;
+printf("\n%dhours%dminutes",hour,min,);
+}   
+min = input / 60;
+sec = input % 60;
+printf("%dhour and %dminutes\n",min,sec);
+return 0;
+}
