5b1ecff6133ed631eafeca2a688367de69ea4e8e
surya220rider <30829445+surya220rider@users.noreply.github.com>
2018-02-09 15:49:24 +0530

    Create 33.c

---
+++ 33.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+int i, spaces;
+char a[30];
+printf("enter the string");
+scanf("%s", a);
+fr(i=0 ; a[0]!="\0" ; i++
+{
+if (a[i]=' ')
+{
+spaces++;
+}
+}
+printf("Number of spaces is %d", spaces);
+printf("Number of words is %d", spaces + 1);
+}
