99121a55f6c61446ad82f77d6d180702ad9477b2
surya220rider <30829445+surya220rider@users.noreply.github.com>
2018-02-09 15:56:13 +0530

    Create 34.c

---
+++ 34.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+char s[100];
+int l,count=1,i;
+scanf("%s",s);
+l=strlen(s);
+for(i=0;i<1,i++0
+{
+if(s[i]=='.')
+count++;
+printf("%d",count);
+else
+printf("%d",count);
+}
+return 0;
+}
