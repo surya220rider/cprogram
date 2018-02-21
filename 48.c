195e4851a8e5e8389af9ed5a8c8095fde777b707
surya220rider <30829445+surya220rider@users.noreply.github.com>
2018-02-21 16:32:08 +0530

    Create 48.c

---
+++ 48.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main() 
+{
+int n,sum=0,i,average;
+scanf("%d",&n);
+int a[1000];
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);	
+}
+for(i=0;i<n;i++)
+{
+sum=sum+a[i];
+}
+average=sum/n;
+printf("%d",average);
+}
