4c8b729aa6937a4173b606faafa839d2dce19056
surya220rider <30829445+surya220rider@users.noreply.github.com>
2018-02-23 16:39:38 +0530

    Create 53.c

---
+++ 53.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int num,temp,sum=0;
+printf("enter the number");
+scanf("%d",&num);
+temp=num;
+while(num!=0)
+{
+temp=num%10;
+sum=sum+temp;
+num=num/10;
+}
+printf("%d",sum);
+getch();
+}
