a2d6326e4134519f62e8ec78a08eb48609981bc7
surya220rider <30829445+surya220rider@users.noreply.github.com>
2018-02-26 16:12:12 +0530

    Create 56.c

---
+++ 56.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+int a;
+char str;
+printf("Enter the string");
+scanf("%d",&a);
+if(((a>='a')||(a<='z'))&&((a>='0')||(a<='100')))
+{
+printf("yes");
+}
+else
+{
+prinf("no");
+}
