f41c01fdc71cebb6968b154c5083991d0e592e54
surya220rider <30829445+surya220rider@users.noreply.github.com>
2018-03-05 16:35:09 +0530

    Create 68.c

---
+++ 68.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("Enter the value of n");
+scanf("%d",&n);
+if(n%7==0)
+{
+printf("the value is multiple of 7");
+}
+else
+{
+printf("the value is not multiple of 7");
+}
+}
