a2c3ad46e3848550e6d7e512f6e2f3d25adc34bd
surya220rider <30829445+surya220rider@users.noreply.github.com>
2018-03-07 16:32:55 +0530

    Create 72.c

---
+++ 72.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int str[34],i;
+printf("enter a string");
+scanf("%s",&str[i]);
+{
+if(str[i]=="a"||str[i]=="e"||str[i]=="i"||str[i]=="o"||str[i]=="u")
+{ 
+printf("yes");
+}
+else
+{
+printf("no");
+}
+getch();
+}
