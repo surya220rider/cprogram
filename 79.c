70a66067235b4c0444144b6ededb4bee33079ad4
surya220rider <30829445+surya220rider@users.noreply.github.com>
2018-03-12 15:58:04 +0530

    79.c

---
+++ 79.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,mul,s;
+printf("enter number a");
+scanf("%d",&a);
+printf("enter number b");
+scanf("%d",&b);
+mul=a*b;
+printf("the product of the 2 numbers is: %d",mul);
+s=sqrt(mul);
+if(m==a)
+{
+printf("the number is a perfect square");
+}
+else
+{
+printf("the number is not a perfect square");
+}
+getch();
+}
