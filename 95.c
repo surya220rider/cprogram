8fb8fdf96547871d4875e57958bd3dc7461ec564
surya220rider <30829445+surya220rider@users.noreply.github.com>
2018-03-23 15:52:01 +0530

    Create 95.c

---
+++ 95.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float p,r,result;
+int s,n;
+scanf("%f %d %f",&p,&n,&r);
+result=((p*n*r)/100);
+s=floor(result);
+printf("%d",si);
+getch();
+} 
